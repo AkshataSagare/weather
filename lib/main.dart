1e78dd55af6a8ed0b53e9a2467ff5d4c25185a65
AkshataSagare <akshatasagare17@gmail.com>
2025-07-18 23:05:17 +0530

    rename project folder

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:weather/core/app_themes/app_themes.dart';
-import 'package:weather/presentations/main_screen.dart';
+import 'package:weather_app/core/app_themes/app_themes.dart';
+import 'package:weather_app/presentations/main_screen.dart';
 
 void main() {
   runApp(const MyApp());
